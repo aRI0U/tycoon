250d79ea67cc3754f29fbe54f8572102f3548b1b
nathan <nathan.lcht@gmail.com>
2018-02-21 18:34:58 +0100

    i like trains

--- src/main/scala/tycoon/ui/StartScreen.scala
+++ src/main/scala/tycoon/ui/StartScreen.scala
@@ -13,22 +13,33 @@ import scalafx.scene.text.Text
 import scalafx.geometry.{Pos, Insets}
 import scalafx.scene.control.Button
 import scalafx.scene.input.MouseEvent
+import scalafx.scene.media._
 import scalafx.scene.image.{Image, ImageView}
 
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.chart.PieChart
 
 class StartScreen extends BorderPane
 {
   private var onStart = new Runnable { def run() {} }
 
+  private var iliketrains = new AudioClip("file:src/main/resources/trains.mp3")
+
   def setOnStart(r : Runnable) = {
     onStart = r
   }
-  val bg = new Image("file:src/main/resources/Train_screen1.jpeg")
+  var bg = new Image("file:src/main/resources/Train_screen1.jpeg")
+  val bg_trains = new Image("file:src/main/resources/trains.jpg")
+
+
   // style = "-fx-background-image"
     //"-fx-background-color: linear-gradient(blue, pink, blue)"
   background = new Background(Array(new BackgroundImage(bg, BackgroundRepeat.NoRepeat, BackgroundRepeat.NoRepeat,
     BackgroundPosition.Center, BackgroundSize.Default)))
   center = new HBox {
+    def changebg() = { super.background = new Background(Array(new BackgroundImage(bg_trains, BackgroundRepeat.NoRepeat, BackgroundRepeat.NoRepeat,
+      BackgroundPosition.Center, BackgroundSize.Default))) }
+
     alignment = Pos.Center
     children = Seq(
       new Button {
@@ -63,6 +74,30 @@ class StartScreen extends BorderPane
                   -fx-font-size: 18px;
                   -fx-effect: dropshadow( one-pass-box , rgba(0,0,0,0.8) , 0, 0.0 , 0 , 1);"""
         onMouseClicked = (e: MouseEvent) => Platform.exit()
+      },
+      new Button {
+        text = "i like trains"
+        margin = Insets(50)
+        minWidth = 200
+        style = """-fx-background-color: pink;
+                  -fx-background-insets: 0,1,2,3;
+                  -fx-background-radius: 3,2,2,2;
+                  -fx-padding: 12 30 12 30;
+                  -fx-width: 300;
+                  -fx-text-fill: purple;
+                  -fx-font-size: 18px;
+                  -fx-effect: dropshadow( one-pass-box , rgba(0,0,0,0.8) , 0, 0.0 , 0 , 1);"""
+        onMouseClicked = (e: MouseEvent) => { changebg() ; iliketrains.play() 
+         }
+      },
+      new PieChart {
+        title = "I like trains"
+        data = ObservableBuffer(
+          PieChart.Data("TRAINS", 20),
+          PieChart.Data("TRAINS", 30),
+          PieChart.Data("TRAINS", 10),
+          PieChart.Data("TRAINS", 40)
+        )
       }
     )
   }
