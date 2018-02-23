059c23209617f15df1ab79a7c9f31a80c63b23d2
rachide <ardoin.thibaud@gmail.com>
2018-02-23 17:53:14 +0100

    creation town

--- src/main/scala/tycoon/ui/CreationGameScreen.scala
+++ src/main/scala/tycoon/ui/CreationGameScreen.scala
@@ -1,6 +1,7 @@
 package tycoon.ui
 
 import tycoon.Game
+import tycoon.DraggableTiledPane
 
 
 import scalafx.Includes._
@@ -13,6 +14,8 @@ import scalafx.scene.text.Text
 import scalafx.geometry.{Pos, Insets, Rectangle2D}
 import scalafx.scene.control.Button
 import scalafx.scene.image.{Image, ImageView}
+import scalafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
+import scala.collection.mutable.ListBuffer
 
 import scalafx.scene.input.MouseEvent
 
@@ -23,6 +26,7 @@ import scala.collection.mutable.HashMap
 
 class CreationGameScreen(var game : Game) extends BorderPane
 {
+  /*
   style = "-fx-background-color: lightblue;"
 
   //Valeur relative au déplacement, à modifier. En pixels.
@@ -36,11 +40,30 @@ class CreationGameScreen(var game : Game) extends BorderPane
     val x  = Math.round( e.getSceneX()).toInt/32
     val y  = Math.round( e.getSceneX()).toInt/32
     return (x,y)
+    
   }
+  */
+   
   center = new BorderPane {
+    final val tileset = new Image("file:src/main/resources/tileset.png")
+    var tiledPane = new DraggableTiledPane(32, 32, tileset)
+
+    style = "-fx-background-color: lightgreen"
+    center = tiledPane
+
+    //Creation of a City 
     onMouseClicked = (e: MouseEvent) => {
-      val (x,y) = mouse_to_box(e)
-      game.create_town (x,y)
+      val (x,y) = tiledPane.pixelToCase(e.getSceneX(),e.getSceneY())
+      /*
+      def f( t :Town) :Boolean =  {if ((t.pos_X,t.pos_Y) == (x,y)) {return true} else return false}
+      if ((x,y) == game.townsList.find(f_)) {*/
+        game.create_town (x,y)
+    }
+
+    onKeyPressed = (k: KeyEvent) => k.code match {
+      case KeyCode.E =>
+        System.out.println("e key was pressed");
+      case _ =>
     }
   }
 }
