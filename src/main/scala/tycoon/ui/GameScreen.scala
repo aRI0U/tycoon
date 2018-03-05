11e499bb0b2b72df38a28f50e612a3bf61dfa57d
nathan <nathan.lcht@gmail.com>
2018-03-05 23:10:22 +0100

    added basics for 'Help' button

--- src/main/scala/tycoon/ui/GameScreen.scala
+++ src/main/scala/tycoon/ui/GameScreen.scala
@@ -14,7 +14,7 @@ import scalafx.scene.layout.{BorderPane, VBox, StackPane, Pane}
 import scalafx.scene.text.Text
 import scalafx.geometry.{Pos, HPos, VPos, Insets, Rectangle2D, Orientation}
 import scalafx.geometry.Orientation._
-import scalafx.scene.control.{Button, Separator, ButtonType, Alert}
+import scalafx.scene.control.{Button, Separator, ButtonType, Alert, TextArea, Label}
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.input.{MouseEvent, KeyEvent}
 
@@ -24,7 +24,7 @@ import scalafx.collections.ObservableBuffer._ // Add, Remove, Reorder, Update
 
 import scalafx.scene.control.{TextField, Button}
 import scalafx.scene.input.MouseEvent
-import scalafx.scene.layout.{BorderPane, HBox, VBox}
+import scalafx.scene.layout.{BorderPane, HBox, VBox, GridPane, Priority}
 
 import scalafx.beans.property.{StringProperty, IntegerProperty}
 import scalafx.beans.binding.Bindings
@@ -214,25 +214,53 @@ class GameScreen(var game : Game) extends BorderPane
 
     bottom = new VBox {
       alignment = Pos.BottomCenter
-      children = Seq(new Button {
-        text = "Quit Game"
-        margin = Insets(10)
 
-        onMouseClicked = (e: MouseEvent) => {
-          val alert = new Alert(Alert.AlertType.Warning) {
-            title = "Careful!"
-            headerText = "If you leave now, your progress will not be saved."
-            contentText = "Are you sure you want to leave?"
-            buttonTypes = Seq(ButtonType.Cancel, ButtonType.Yes)
-          }
+      children = new HBox {
+        children = Seq(
+          new Button {
+            text = "Help"
+            margin = Insets(10)
 
-          val result = alert.showAndWait()
-          result match {
-            case Some(ButtonType.Yes) => Platform.exit()
-            case _                   => ()
+            val label = new Label("TODO detailed list of implemented features")
+
+            val expContent = new GridPane {
+              maxWidth = Double.MaxValue
+              add(label, 0, 0)
+              // texte avec du gras et tout :
+              // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/text/TextFlow.html
+            }
+
+            onMouseClicked = (e: MouseEvent) => {
+              new Alert(Alert.AlertType.Information) {
+                title = "Help"
+                headerText = "The Tycoon Game - V 1.0.0 Beta"
+                contentText = "See below for details."
+                buttonTypes = Seq(new ButtonType("Got it!"))
+                dialogPane().expandableContent = expContent
+              }.showAndWait()
+            }
+          },
+          new Button {
+            text = "Quit Game"
+            margin = Insets(10)
+
+            onMouseClicked = (e: MouseEvent) => {
+              val alert = new Alert(Alert.AlertType.Warning) {
+                title = "Careful!"
+                headerText = "If you leave now, your progress will not be saved."
+                contentText = "Are you sure you want to leave?"
+                buttonTypes = Seq(ButtonType.Cancel, ButtonType.Yes)
+              }
+
+              val result = alert.showAndWait()
+              result match {
+                case Some(ButtonType.Yes) => Platform.exit()
+                case _                   => ()
+              }
+            }
           }
-        }
-      })
+        )
+      }
     }
 
     //onMouseClicked = (e: MouseEvent) => { requestFocus() }
